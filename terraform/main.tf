ea34635763d9fa1e90019c85ea3d4dfb8205739a
Yulia Savchenko <savcenkoulia138@gmail.com>
2026-09-06 20:23:34 +0300

    Install Docker and deploy calculator

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,8 @@ resource "aws_instance" "calc_server" {
   ami = "ami-0aba19e56f3eaec05"
   instance_type = "t3.micro"
 
+  key_name      = "aws-ubuntu-key"
+
   vpc_security_group_ids = [aws_security_group.calc_sg.id]
 
   tags = {
